0b94604e885a30324fc3870756d2d7d77ef96c1d
pius712 <aksfbsgnlfls@gmail.com>
2024-03-10 16:35:18 +0900

    fix: configuration property scan 추가

--- boilerplate-core-api/src/main/kotlin/com/pius/boilerplate/KtApplication.kt
+++ boilerplate-core-api/src/main/kotlin/com/pius/boilerplate/KtApplication.kt
@@ -1,8 +1,10 @@
 package com.pius.boilerplate
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 class KtApplication
 
